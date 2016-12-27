cf84fbc1ca73c00705cc396ca7c7833e1e51854f
Sarah Waldie <sareliz126@gmail.com>
2016-12-27 16:27:12 -0500

    added lite server

--- gulpfile.js
+++ gulpfile.js
@@ -26,38 +26,37 @@ gulp.task('sass', function () {
     .pipe(browserSync.stream())
 });
 
-gulp.task('browser-sync', ['nodemon'], function () {
-  browserSync({
-    proxy: "localhost:8080",  // local node app address
-    port: 9000,  // use *different* port than above
-    notify: true
-  });
-});
+// gulp.task('browser-sync', ['nodemon'], function () {
+//   browserSync({
+//     proxy: "localhost:8080",  // local node app address
+//     port: 9000,  // use *different* port than above
+//     notify: true
+//   });
+// });
 
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
-    script: 'server.js',
-    ignore: [
-      'gulpfile.js',
-      'node_modules/'
-    ]
-  })
-    .on('start', function () {
-      if (!called) {
-        called = true;
-        cb();
-      }
-    })
-    .on('restart', function () {
-      setTimeout(function () {
-        reload({ stream: false });
-      }, 1000);
-    });
-});
+// gulp.task('nodemon', function (cb) {
+//   var called = false;
+//   return nodemon({
+//     script: 'server.js',
+//     ignore: [
+//       'gulpfile.js',
+//       'node_modules/'
+//     ]
+//   })
+//     .on('start', function () {
+//       if (!called) {
+//         called = true;
+//         cb();
+//       }
+//     })
+//     .on('restart', function () {
+//       setTimeout(function () {
+//         reload({ stream: false });
+//       }, 1000);
+//     });
+// });
 
-gulp.task('default', ['browser-sync'], function () {
-  gulp.watch([htmlSource], reload);
+gulp.task('default', function () {
   gulp.watch(sassSources, ['sass'])
 });
 
